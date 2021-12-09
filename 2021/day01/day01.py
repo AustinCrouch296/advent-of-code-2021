04b41d360f8d822190383d323fcc02957d65026f
Austin Crouch <AustinCrouch296@gmail.com>
2021-12-09 18:32:55 -0500

    Update day01.py

--- 2021/day01/day01.py
+++ 2021/day01/day01.py
@@ -1,1 +1,15 @@
-#
+file = open("input.txt", "r") #read mode
+line = file.readlines()
+
+count = 0
+num = int(line[0])
+
+for i in range(2000):
+    nextnum = int(line[i])
+    
+    if num < nextnum:
+        count+= 1
+    
+    num = nextnum
+
+print(count)
